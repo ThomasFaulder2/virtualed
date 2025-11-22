d3a9ef55fcd8c8cbbaa09d01e6279d0f36913a37
“ThomasFaulder2” <tfauler2@bigpond.com>
2025-11-22 13:21:18 +1030

    message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const { OpenAI } = require("openai");
-
+const fetch = require("node-fetch");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
